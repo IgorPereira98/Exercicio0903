7da065b4bdbbcd1283a717f057a7c4319e15a906
06-11 <06-11>
2017-03-09 14:41:17 -0300

    Término do Bootstrap

--- index.php
+++ index.php
@@ -1,21 +1,45 @@
+
 <!DOCTYPE html>
 <html>
    <head>
         <title></title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
+        <link href="bootstrap.min.css" rel="stylesheet">
   </head>
 <body>
-    <div id="area">
-    <form id="formulario" autocomplete="off">
-      <fieldset>
-        <legend>Formulário</legend>
-        <label>Campo1:</label><input class="campo_numero" type="number"><br>
-        <label>Campo2:</label><input class="campo_email" type="password"><br>
-        <input class="btn_submit" type="submit" value="Enviar">
-      </fieldset>
-    </form>
+   <form class="form-horizontal">
+  <fieldset>
+    <legend><b>FORMULÁRIO</b></legend>
+    <div class="form-group">
+      <label for="inputvalor1" class="col-lg-1 control-label">Valor 1</label>
+      <div class="col-lg-5">
+        <input type="text" class="form-control" id="inputvalor1" placeholder="Digite um número">
+      </div>
+      </div>
+
+      <div class="form-group">
+      <label for="inputvalor2" class="col-lg-1 control-label">Valor 2</label>
+      <div class="col-lg-5">
+        <input type="text" class="form-control" id="inputvalor2" placeholder="Digite um número">
+      </div>
+      </div>
+
+      <div class="form-group">
+      <label for="inputresult" class="col-lg-1 control-label">Resultado</label>
+      <div class="col-lg-5">
+        <input type="text" class="form-control" id="inputresult">
+      </div>
+      </div>
+
+      <div class="form-group">
+      <div class="col-lg-4 col-lg-offset-1">
+        <button type="submit" class="btn btn-primary">ENVIAR</button>
+      </div>
     </div>
+      
+    </fieldset>
+    </form>
+   
 </body>
 </html>
