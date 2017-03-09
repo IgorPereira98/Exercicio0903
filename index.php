704ad139ff8505613146e82d77acaae894bbb820
Igor <igor_rafael25@outlook.com>
2017-03-09 13:45:52 -0300

    Primeiro commit index criado

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+   <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+  </head>
+<body>
+  <div id="area">
+    <form id="formulario" autocomplete="off">
+      <fieldset>
+        <legend>Formulário</legend>
+        <label>Numero1:</label><input class="campo_1" type="number"><br>
+        <label>Numero2:</label><input class="campo_2" type="number"><br>
+        <input class="btn_submit" type="submit" value="Enviar">
+      </fieldset>
+    </form>
+  </div>
+</body>
+</html>
